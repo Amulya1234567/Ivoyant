723931c1b4e77564afccb4c432a45d0eb95e83d0
Amulya1234567 <amulyatm940@gmail.com>
2025-03-17 23:45:17 +0530

    Day10:SpringJDBC

---
+++ src/main/java/com/spring/Dml.java
@@ -0,0 +1,57 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class Dml
+{
+    public static void main( String[] args )
+    {
+
+//        int sid=11;
+//        String sname="deepak";
+//        int marks=88;
+        ApplicationContext context=new AnnotationConfigApplicationContext(SpringConfigFile.class);
+
+        JdbcTemplate jdbcTemplate=context.getBean(JdbcTemplate.class);
+//        Insert operation
+
+//        String query="INSERT INTO Student VALUES(?,?,?)";
+//        int count=jdbcTemplate.update(query, sid, sname, marks);
+//        if(count >0){
+//            System.out.println("insersion success");
+//        }else{
+//            System.out.println("insersion failed");
+//        }
+
+
+//        update operation
+
+//        int MARKS=67;
+//
+//        String query="UPDATE Student SET MARKS=? WHERE SID=1";
+//        int count=jdbcTemplate.update(query,MARKS);
+//        if(count>0){
+//            System.out.println("update success");
+//        }else{
+//            System.out.println("update is not success");
+//        }
+
+
+//        Delete operation
+        int SID=1;
+        String query="DELETE FROM Student WHERE SID=?";
+        int count=jdbcTemplate.update(query,SID);
+        if(count>0){
+            System.out.println("delete success");
+        }else{
+            System.out.println("delete is not success");
+        }
+
+    }
+}
---
+++ src/main/java/com/spring/Student.java
@@ -0,0 +1,11 @@
+package com.spring;
+
+import lombok.Data;
+import lombok.Setter;
+
+@Data
+public class Student {
+    private int sid;
+    private String sname;
+    private int marks;
+}
